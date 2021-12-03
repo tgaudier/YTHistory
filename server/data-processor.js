1fab608d8652b5149ccf3927e41097a92f0b4da0
Thibault Gaudier <thibault.gaudier78@gmail.com>
2021-12-03 15:03:47 +0100

    Use of json new history file (url only)

--- server/data-processor.js
+++ server/data-processor.js
@@ -1,48 +1,16 @@
 
 const fs = require('fs')
-const { JSDOM } = require( "jsdom" );
 
-function dataloader (filename) {
-	const strDom = fs.readFileSync(filename, "utf-8")
-	return new JSDOM (strDom)
-}
-
-function dataparser (domText) {
-
-	let dom = new JSDOM (domText)
-
-	let jsonData = []
-	let content_cells = dom.window.document.querySelectorAll('.mdl-grid')
-
-	for (i = 0; i < content_cells.length; i++) {
-		let channelName = ""
-		let videoId = ""
-		let watchDate = ""
-
-		try {
-			channelName = content_cells[i].querySelectorAll('a')[1].innerHTML
-		} catch (TypeError) {
-			//console.log("No channel for element " + i)
-		}
-
-		try {
-			videoId = content_cells[i].querySelectorAll('a')[0].getAttribute('href').split('=')[1]
-		} catch (TypeError) {
-			//console.log("No video for element " + i)
-		}
-
-		try {
-			watchDate = content_cells[i].lastChild.innerHTML
-			console.log(watchDate)
-		} catch (TypeError) {
-			console.log("No watch date for element " + i)
-		}
-
-		jsonData.push(channelName)
+function dataloader (str_data) {
+	const jsonData = JSON.parse(str_data)
+	let elts = []
+	
+	for (elt of jsonData) {
+		elts.push(elt.titleUrl)
 	}
-
-	return jsonData
+	return elts
 }
+
 
 function dataextractor (jsonData) {
 	let dict = {}
@@ -61,6 +29,5 @@ function dataextractor (jsonData) {
 
 module.exports = {
 	dataloader: dataloader,
-	dataparser: dataparser,
 	dataextractor: dataextractor
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dataprocessor = require('./data-processor.js')
 const ejs = require('ejs')
 
-const api_key = require('./private/api-key.js')
+// const api_key = require('./private/api-key.js')
 
 const app = express()
 const port = 8080
@@ -16,7 +16,7 @@ app.use(express.static('public'))
 app.post('/loadfile', (req, res) => {
 	console.log("Received file !")
 	let file = req.files.ythistory
-	let json_data = dataprocessor.dataparser(file.data.toString("utf-8"))
+	let json_data = dataprocessor.dataloader(file.data.toString("utf-8"))
 	let dict = dataprocessor.dataextractor(json_data)
 
 	res.render("main.ejs", {dict: dict})
